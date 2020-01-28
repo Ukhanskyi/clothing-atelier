e16a2785e84fc872aaa2b3dc6832523f2d6385ea
Ukhanskyi <ukhanskyi@gmail.com>
2020-01-28 17:26:29 +0200

    Add update and destroy posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,19 +4,46 @@ class PostsController < ApplicationController
     @post = Post.all
   end
 
-  def new; end
+  def new
+    @post = Post.new
+  end
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+
+    @post.destroy
+    redirect_to posts_path
   end
 
   def create
     # render plain: params[:post].inspect
 
     @post = Post.new(post_params)
-    @post.save
-    redirect_to @post
+
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
     end
+  end
 
   private
 
@@ -24,3 +51,4 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body)
   end
 end
+
