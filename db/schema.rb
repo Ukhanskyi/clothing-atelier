c5fd9bec02963f8ae99b2f4e7e692b4df92952cd
Ukhanskyi <ukhanskyi@gmail.com>
2020-02-06 14:17:48 +0200

    Add date and edit profile fix type price

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_28_074432) do
+ActiveRecord::Schema.define(version: 2020_02_06_094721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clothings", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+    t.string "collar"
+    t.string "size"
+    t.string "sleeve"
+    t.string "length"
+    t.decimal "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "responces", force: :cascade do |t|
+    t.string "username"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.text "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,6 +58,15 @@ ActiveRecord::Schema.define(version: 2020_01_28_074432) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider", limit: 50, default: "", null: false
+    t.string "uid", limit: 500, default: "", null: false
+    t.boolean "admin", default: false
+    t.string "name"
+    t.string "surname"
+    t.boolean "gender"
+    t.string "phone", limit: 13
+    t.string "address"
+    t.string "size_clothing"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
