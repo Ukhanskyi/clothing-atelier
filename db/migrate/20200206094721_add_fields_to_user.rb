466d36c2527326ed178f725820e3397d3cce47e3
ukhanskyi <ukhanskyi@gmail.com>
2020-02-06 16:03:05 +0200

    Add date and edit profile fix type price (#3)

---
+++ db/migrate/20200206094721_add_fields_to_user.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :surname, :string
+    add_column :users, :gender, :boolean
+    add_column :users, :phone, :string, limit: 13
+    add_column :users, :address, :string
+    add_column :users, :size_clothing, :string
+  end
+end
