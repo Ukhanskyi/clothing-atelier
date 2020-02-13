ebfb61c894fd675a8d736431980008eacc8e6561
ukhanskyi <ukhanskyi@gmail.com>
2020-02-13 23:20:03 +0200

    Add meiler for orders with dotenv and add config for prodaction (#5)
    Check on heroku

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,12 @@ class Order < ApplicationRecord
   has_one :detail, dependent: :destroy
 
   accepts_nested_attributes_for :detail
+
+  after_create :order_email
+
+  private
+
+  def order_email
+    UserMailer.order_email(self).deliver_now
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,31 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+
+
+  # Google Mailer
+  # config.action_mailer.delivery_method = :sendmail
+  #
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'clothing-atelier.herokuapp.com' }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default charset: "utf-8"
+
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'smtp.gmail.com',
+      user_name:            ENV['MAILER_USER_NAME'],
+      password:             ENV['MAILER_PASSWORD'],
+      authentication:       'plain',
+      enable_starttls_auto: true,
+      openssl_verify_mode: "none",
+  }
 end
