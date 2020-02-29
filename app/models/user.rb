11979f67ca6ae8174d515030e6e6a6f706888009
Ukhanskyi <ukhanskyi@gmail.com>
2020-02-29 14:41:13 +0200

    Add validation into model and add test for post validation and show link newpost only for admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,25 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :orders
+
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+  validates :password, presence: true,
+                       confirmation: true,
+                       length: { within: 6..40 },
+                       on: :create
+
+  validates :password, confirmation: true,
+                       length: { within: 6..40 },
+                       allow_blank: true,
+                       on: :update
+
+  validates :name, presence: true, length: { within: 3..25 }
+
+  validates :surname, presence: true, length: { within: 3..50 }
+
+  validates :phone, presence: { message: 'Write your number in correct form!' },
+                    numericality: true,
+                    length: { within: 10...15 }
+  validates :address, presence: true, length: { maximum: 100 }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,5 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    described_class.new(title: 'Test Title',
+                        body: 'Lorem ipsum text for body')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a title' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a body' do
+    subject.body = nil
+    expect(subject).to_not be_valid
+  end
 end
