a61791ed1c89c3d2c3067c079c15a7cbb0f6e028
Mykola Ukhanskyi <ukhanskyi@gmail.com.com>
2020-04-04 10:51:37 +0300

    Fix bug with gender

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,25 +25,27 @@ class PostsController < ApplicationController
   def edit; end
 
   def update
-    if @post.update(post_params)
-      format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-      format.json { render :show, status: :ok, location: @post }
-    else
-      format.html { render :edit }
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { render :show, status: :ok, location: @post }
+      else
+        format.html { render :edit }
+      end
     end
   end
 
   def destroy
     @post.destroy
     respond_to do |format|
-      format.html do redirect_to posts_path,
-                     notice: 'Post was successfully destroyed.'
-      end
+      format.html { redirect_to posts_path, notice: 'Post was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   def create
+    @post = Post.new(post_params)
+
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Order was successfully created.' }
