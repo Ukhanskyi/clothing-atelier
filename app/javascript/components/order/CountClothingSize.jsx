e04379700bc8c204d1a62ee90b984886312571fe
Mykola Ukhanskyi <ukhanskyi@gmail.com.com>
2020-04-22 11:43:19 +0300

    Add input form for calculator

--- app/javascript/components/order/CountClothingSize.jsx
+++ app/javascript/components/order/CountClothingSize.jsx
@@ -1,22 +1,47 @@
 import React from 'react'
 import Collapsible from '../Collapsible'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
 
 const CountClothingSize = () => (
   <div>
-    <div>
-      <Collapsible button={
-        <div class="row justify-content-end">
-          <button className="menu btn btn-light"> Calculate my size </button>
-        </div>
-      }>
-        <div className="container">
-          <h4 class="row justify-content-center"> You can calculate your size here </h4>
+    <br />
+    <Collapsible button={
+      <div>
+        <button className="menu btn btn-light"> Calculate my size </button>
+      </div>
+    }>
+      <div className="container col-sm-9">
+        <br />
+        <h4 className="row justify-content-center"> You can calculate your size here </h4>
+        <br/>
+        <form>
+          <div className=" row group mt-1">
+            <h6 className="my-auto col-sm-3">Height: </h6>
+            <input type="number" name="height" placeholder='In centimeters' className="form-control col-md-6" />
+          </div>
 
-        </div>
-      </Collapsible>
-    </div>
+          <div className=" row group mt-1">
+            <h6 className="my-auto col-sm-3">Breast circumference: </h6>
+            <input type="number" name="breast" placeholder='In centimeters' className="form-control col-md-6" />
+          </div>
+
+          <div className=" row group mt-1">
+            <h6 className="my-auto col-sm-3">Waist circumference: </h6>
+            <input type="number" name="waist" placeholder='In centimeters' className="form-control col-md-6" />
+          </div>
+
+          <div className=" row group mt-1">
+            <h6 className="my-auto col-sm-3">Tight circumference: </h6>
+            <input type="number" name="tight" placeholder='In centimeters' className="form-control col-md-6" />
+          </div>
+
+          <div>
+            <input type="submit" value="Calculate" className="btn btn-outline-primary mt-2"/>
+          </div>
+
+        </form>
+        <br/>
+      </div>
+    </Collapsible>
   </div>
 )
 
--- app/javascript/components/order/NewOrder.jsx
+++ app/javascript/components/order/NewOrder.jsx
@@ -1,40 +1,130 @@
 import React from 'react'
+import { Form, Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap"
 import CountClothingSize from './CountClothingSize.jsx'
 
 const NewOrder = ({ current_user }) => (
-  <div className="content w-75 p-3">
-    <div>
+  <div className=" row content">
+    <div className="col-lg">
       <h3 className="row justify-content-center">New order</h3>
       <br/>
 
-      <form>
-        <div className=" row group mt-1">
-          <h6 className="my-auto col-sm-3">Name: </h6>
-          <input type="text" name="name" defaultValue={current_user.name} className="form-control col-md-6" />
+      <div className="row">
+        <div className="col-sm-11">
+          <form>
+            <div className=" row group mt-1">
+              <h6 className="my-auto col-sm-3">Name: </h6>
+              <input type="text" name="name" defaultValue={current_user.name} className="form-control col-md-6" />
+            </div>
+
+            <div className=" row group mt-1">
+              <h6 className="my-auto col-sm-3">Surame: </h6>
+              <input type="text" name="surname" defaultValue={current_user.surname} className="form-control col-md-6" />
+            </div>
+
+            <div className=" row group mt-1">
+              <h6 className="my-auto col-sm-3">Email: </h6>
+              <input type="text" name="email" defaultValue={current_user.email} className="form-control col-md-6" />
+            </div>
+
+            <div className=" row group mt-1">
+              <h6 className="my-auto col-sm-3">Phone: </h6>
+              <input type="number" name="phone" defaultValue={current_user.phone} className="form-control col-md-6" />
+            </div>
+
+            <div className=" row group mt-1">
+              <h6 className="my-auto col-sm-3">Address: </h6>
+              <input type="text" name="address" defaultValue={current_user.address} className="form-control col-md-6" />
+            </div>
+
+            <div className="row group mt-1">
+              <h6 className="my-auto col-sm-3">Your gender: </h6>
+              <input type="radio" value="male" name="gender" className="mt-1" /> Male
+            <input type="radio" value="female" name="gender" className="ml-3 mt-1" /> Female
+          </div>
+
+            <div className=" row group mt-1">
+              <h6 className="my-auto col-sm-3">Clothing name: </h6>
+              <input type="text" name="clothing_name" placeholder='Please enter clothing name' className="form-control col-md-6" />
+            </div>
+
+            <div className=" row group mt-1">
+              <Form.Label className="my-auto col-sm-3">Colour:</Form.Label>
+              <Form.Control as="select" className="form-control col-md-6" custom>
+                <option>White</option>
+                <option>Black</option>
+                <option>Blue</option>
+                <option>Red</option>
+                <option>Purple</option>
+                <option>Green</option>
+                <option>Orange</option>
+                <option>Pink</option>
+                <option>Yellow</option>
+                <option>Brown</option>
+                <option>Grey</option>
+              </Form.Control>
+            </div>
+
+            <div className=" row group mt-1">
+              <Form.Label className="my-auto col-sm-3">Collar: </Form.Label>
+              <Form.Control as="select" className="form-control col-md-6" custom>
+                <option>Basic</option>
+                <option>Polo</option>
+                <option>V-neck</option>
+              </Form.Control>
+            </div>
+
+            <div className=" row group mt-1">
+              <Form.Label className="my-auto col-sm-3">Sleeve: </Form.Label>
+              <Form.Control as="select" className="form-control col-md-6" custom>
+                <option>Short</option>
+                <option>Middle</option>
+                <option>Long</option>
+              </Form.Control>
+            </div>
+
+            <div className=" row group mt-1">
+              <Form.Label className="my-auto col-sm-3">Length: </Form.Label>
+              <Form.Control as="select" className="form-control col-md-6" custom>
+                <option> Mini </option>
+                <option> Above knee </option>
+                <option> Knee length </option>
+                <option> Cocktail </option>
+                <option> Midi </option>
+                <option> Maxi </option>
+                <option> Floor length </option>
+              </Form.Control>
+            </div>
+
+            <div className=" row group mt-1">
+              <Form.Label className="my-auto col-sm-3">Size: </Form.Label>
+              <Form.Control as="select" className="form-control col-md-6" custom>
+                <option> XS </option>
+                <option> S </option>
+                <option> M </option>
+                <option> L </option>
+                <option> XL </option>
+                <option> XXL </option>
+              </Form.Control>
+            </div>
+
+            <div className=" row group mt-1">
+              <h6 className="my-auto col-sm-3">Price: </h6>
+              <input type="number" name="price" className="form-control col-md-6" />
+            </div>
+
+            <div>
+              <input type="submit" value="Make order" className="btn btn-outline-primary mt-2"/>
+            </div>
+          </form>
         </div>
 
-        <div className=" row group mt-1">
-          <h6 className="my-auto col-sm-3">Surame: </h6>
-          <input type="text" name="surname" defaultValue={current_user.surname} className="form-control col-md-6" />
-        </div>
+      <br/>
+      <br/>
 
-        <div className=" row group mt-1">
-          <h6 className="my-auto col-sm-3">Email: </h6>
-          <input type="text" name="email" defaultValue={current_user.email} className="form-control col-md-6" />
-        </div>
-
-        <div className=" row group mt-1">
-          <h6 className="my-auto col-sm-3">Phone: </h6>
-          <input type="number" name="phone" defaultValue={current_user.phone} className="form-control col-md-6" />
-        </div>
-
-        <div className=" row group mt-1">
-          <h6 className="my-auto col-sm-3">Address: </h6>
-          <input type="text" name="address" defaultValue={current_user.address} className="form-control col-md-6" />
-        </div>
-      </form>
-
-      <CountClothingSize />
+      <div className="col-lg ">
+        <CountClothingSize />
+      </div>
+    </div>
 
     </div>
   </div>
